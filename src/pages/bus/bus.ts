058297c43d888a478621ff3ce4e170a1a2cfd605
arnaudbarre <arnaudbarremn@gmail.com>
2017-07-26 22:07:26 +0200

    fix display schedules

--- src/pages/bus/bus.ts
+++ src/pages/bus/bus.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Http } from '@angular/http';
 
@@ -8,19 +8,26 @@ import 'rxjs/add/operator/toPromise';
   selector: 'page-bus',
   templateUrl: 'bus.html'
 })
-export class BusPage {
+export class BusPage implements OnInit {
 
   times = [];
 
   constructor(public navCtrl: NavController, private http: Http) {}
 
+  ngOnInit(): void {
+    this.getBus("C6");
+  }
+
   getBus(type : string): void {
     this.http.get('tan/ewp/tempsattente.json/CTRE')
       .toPromise()
       .then(response => {
-         this.times = response.json().data
+         this.times = response.json()
           .filter(schedule => schedule.ligne.numLigne === type && schedule.sens === 2)
           .map(schedule => schedule.temps);
+         if (this.times.length === 0) {
+           this.times.push('Pas de bus prochainement')
+         }
       })
       .catch(error => console.error(error));
   }
