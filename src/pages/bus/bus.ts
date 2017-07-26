5201ef54608a31435fba6df4c2df7ae69209adae
DESKTOP-8CB3OHD\tombr <tom.brouder@hotmail.fr>
2017-07-26 02:12:51 +0200

    Start display TAN schedules

--- src/pages/bus/bus.ts
+++ src/pages/bus/bus.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'page-bus',
@@ -7,8 +10,19 @@ import { NavController } from 'ionic-angular';
 })
 export class BusPage {
 
-  constructor(public navCtrl: NavController) {
+  times = [];
 
+  constructor(public navCtrl: NavController, private http: Http) {}
+
+  getBus(type : string): void {
+    this.http.get('tan/ewp/tempsattente.json/CTRE')
+      .toPromise()
+      .then(response => {
+         this.times = response.json().data
+          .filter(schedule => schedule.ligne.numLigne === type && schedule.sens === 2)
+          .map(schedule => schedule.temps);
+      })
+      .catch(error => console.error(error));
   }
 
 }
