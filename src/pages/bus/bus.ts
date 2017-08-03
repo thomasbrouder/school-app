3dd55e4e7102745ee481a1693e488b13db5e48a4
DESKTOP-8CB3OHD\tombr <tom.brouder@hotmail.fr>
2017-08-03 23:07:24 +0200

    Add Arrêts liste

--- src/pages/bus/bus.ts
+++ src/pages/bus/bus.ts
@@ -10,6 +10,10 @@ import 'rxjs/add/operator/toPromise';
 export class BusPage implements OnInit {
   times = [];
   sens = 2;
+  ligne="C6";
+  arretSelectione={id:'CTRE', nom:'Chantrerie'};
+  arrets=[{id:'CTRE', nom:'Chantrerie'},{id:'BJOI', nom:'Beaujoire'},{id:'COCH', nom:'Cochard'},
+    {id:'FOCH', nom:'Foch'},{id:'CRQU', nom:'Place du Cirque'},{id:'FACU', nom:'Facultés'}];
 
   constructor(private http: Http) {}
 
@@ -18,11 +22,12 @@ export class BusPage implements OnInit {
   }
 
   getBus(): void {
-    this.http.get('tan/ewp/tempsattente.json/BJOI')
+    console.log(this.arretSelectione);
+    this.http.get('tan/ewp/tempsattente.json/'+this.arretSelectione.id)
       .toPromise()
       .then(response => {
          this.times = response.json()
-          .filter(schedule => schedule.ligne.numLigne === "C6" && schedule.sens === this.sens)
+          .filter(schedule => schedule.ligne.numLigne === this.ligne && schedule.sens === this.sens)
           .map(schedule => schedule.temps);
          if (this.times.length === 0) {
            this.times.push('Pas de bus prochainement (>1h) :(')
